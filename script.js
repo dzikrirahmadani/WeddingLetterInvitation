d5caa348e6f6e9763a79489a2ddfd423d7e9581f
unknown <dzikrirahmadani211@gmail.com>
2022-10-12 14:05:32 +0700

    99%

--- script.js
+++ script.js
@@ -239,9 +239,27 @@ function changeTimelinePosition () {
 }
 
 // NAV JS
-$(document).ready(function() {
-    $('li').on('click', function(){
-        $(this).siblings().removeClass('active');
-        $(this).addClass('active');
-    })
+// $(document).ready(function() {
+//     $('li').on('click', function(){
+//         $(this).siblings().removeClass('active');
+//         $(this).addClass('active');
+//     })
+// });
+
+const navLi = document.querySelectorAll('nav ul li a');
+const sections = document.querySelectorAll('section');
+
+
+window.addEventListener('scroll', () => {
+    let current = '';
+    sections.forEach(section => {
+        let sectionTop = section.offsetTop;
+        if(scrollY >= sectionTop){
+            current = section.getAttribute('id');
+        }
+    });
+    navLi.forEach(li => {
+        li.classList.remove('active');
+        document.querySelector('nav ul li a[href*= '+ current +']').classList.add('active');
+    });
 });
